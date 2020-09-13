5dfb3e94198f85d8cfbc939964a58cea5e96b506
josh-gree <joshua.greenhalgh@heyjobs.de>
2020-09-13 14:30:23 +0200

    add rds

--- infra/rds.py
+++ infra/rds.py
@@ -1,8 +1,47 @@
-from aws_cdk.core import Construct, RemovalPolicy
-from aws_cdk.aws_s3 import Bucket
+import os
+
+from aws_cdk.core import Construct, SecretValue, CfnOutput
+from aws_cdk.aws_rds import DatabaseInstance, DatabaseInstanceEngine
+from aws_cdk.aws_ec2 import (
+    InstanceClass,
+    InstanceSize,
+    InstanceType,
+    SecurityGroup,
+    Peer,
+    Port,
+    SubnetType,
+)
+
+from vpc import Vpc
+
+PASSWORD = os.environ["RDS_PASSWORD"]
+USER = os.environ["RDS_USER"]
+DATABASE = os.environ["RDS_DATABASE"]
+PORT = 5432
 
 
 class Rds(Construct):
-    def __init__(self, scope: Construct, id: str, vpc, **kwargs) -> None:
+    def __init__(self, scope: Construct, id: str, vpc: Vpc, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
-        b = Bucket(self, "b", removal_policy=RemovalPolicy.DESTROY)
+
+        rds = DatabaseInstance(
+            self,
+            "rds",
+            master_username=USER,
+            master_user_password=SecretValue(PASSWORD),
+            database_name=DATABASE,
+            vpc=vpc.vpc,
+            engine=DatabaseInstanceEngine.POSTGRES,
+            port=PORT,
+            instance_type=InstanceType.of(InstanceClass.BURSTABLE3, InstanceSize.MICRO),
+            security_groups=[vpc.sg_rds],
+            vpc_subnets={
+                "subnet_type": SubnetType.PUBLIC
+            },  # Anyone can access for now! REMOVE THIS
+        )
+
+        CfnOutput(
+            self,
+            "rds_address",
+            value=f"postgres://{USER}:{PASSWORD}@{rds.db_instance_endpoint_address}:{PORT}/{DATABASE}",
+        )
--- infra/vpc.py
+++ infra/vpc.py
@@ -1,8 +1,28 @@
 from aws_cdk.core import Construct, RemovalPolicy
-from aws_cdk.aws_s3 import Bucket
+from aws_cdk.aws_ec2 import Vpc as _Vpc, Peer, Port, SecurityGroup
 
 
 class Vpc(Construct):
+    @property
+    def vpc(self):
+        return self.vpc_
+
+    @property
+    def sg_rds(self):
+        return self.sg_rds_
+
     def __init__(self, scope: Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
-        b = Bucket(self, "b", removal_policy=RemovalPolicy.DESTROY)
+        self.vpc_ = _Vpc(self, "vpc", max_azs=2)
+
+        # rds security group #
+        ######################
+        RDS_PORT = 5432
+        self.sg_rds_ = SecurityGroup(self, "sg_rds", vpc=self.vpc_)
+        self.sg_rds_.add_ingress_rule(
+            peer=Peer.ipv4("10.0.0.0/16"), connection=Port.tcp(RDS_PORT)
+        )
+        # Anyone can access for now! REMOVE THIS
+        self.sg_rds_.add_ingress_rule(
+            peer=Peer.any_ipv4(), connection=Port.tcp(RDS_PORT)
+        )
