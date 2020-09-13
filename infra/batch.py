645c839390190288fef0fcec513abbab685641dd
josh-gree <joshua.greenhalgh@heyjobs.de>
2020-09-13 14:20:47 +0200

    remove the test s3 buckets

--- infra/batch.py
+++ infra/batch.py
@@ -5,4 +5,3 @@ from aws_cdk.aws_s3 import Bucket
 class Batch(Construct):
     def __init__(self, scope: Construct, id: str, vpc, rds, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
-        b = Bucket(self, "b", removal_policy=RemovalPolicy.DESTROY)
--- infra/cluster.py
+++ infra/cluster.py
@@ -5,4 +5,3 @@ from aws_cdk.aws_s3 import Bucket
 class Cluster(Construct):
     def __init__(self, scope: Construct, id: str, vpc, rds, batch, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
-        b = Bucket(self, "b", removal_policy=RemovalPolicy.DESTROY)
